c68a92b84e9e20af6a7bb22623de35c430f7a4c4
fahimjoon <93906241+fahimjoon@users.noreply.github.com>
2021-11-08 12:24:05 +0330

    Add files via upload

---
+++ Client1.java
@@ -0,0 +1,10 @@
+import ir.pint.soltoon.soltoongame.client.ClientRunner;
+
+/**
+ * Created by amirkasra on 12/31/2017 AD.
+ */
+public class Client1 {
+	public static void main(String[] args) {
+		ClientRunner.run(Scenario7Soltoon.class);
+	}
+}
